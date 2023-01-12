c344c455b07fce6b4333debef5161243f82f7ec8
Nematullah Omid <98613991+nomid03@users.noreply.github.com>
2023-01-12 11:36:31 +0100

    add more file

---
+++ lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,26 @@
+
+    // TODO: define the 'expectedMinutesInOven()' method
+
+    // TODO: define the 'remainingMinutesInOven()' method
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+    
+
+public class Lasagna {
+    private static final int EXPECTED_OVEN_TIME = 40;
+    private static final short LAYER_TIME_PREPARE = 2;
+    public int remainingMinutesInOven(int currentMinutesInOven) {
+        return EXPECTED_OVEN_TIME - currentMinutesInOven;
+    }
+    public int expectedMinutesInOven() {
+        return EXPECTED_OVEN_TIME;
+    }
+    public int preparationTimeInMinutes(int layers) {
+        return LAYER_TIME_PREPARE * layers;
+    }
+    public int totalTimeInMinutes(int layers, int currentMinutesInOven) {
+        return preparationTimeInMinutes(layers) + currentMinutesInOven;
+    }
+}
---
+++ micro-blog/src/main/java/MicroBlog.java
@@ -0,0 +1,7 @@
+import java.nio.charset.StandardCharsets;
+
+class MicroBlog {
+    public String truncate(String input) {
+        return input.length() < 6 ? input : input.substring(0, input.offsetByCodePoints(0, 5));
+    }
+}
