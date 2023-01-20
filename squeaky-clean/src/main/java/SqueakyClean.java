938cbb58c9de03931683c141d4d34f09a4f4370b
Nematullah Omid <98613991+nomid03@users.noreply.github.com>
2023-01-20 09:27:10 +0100

    SqueakyClean is commleted

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,23 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        var mystring  = new StringBuilder();
+        boolean mybloon = false;
+        for (char c: identifier.toCharArray()) {
+            if (c == ' ') {
+                mystring.append('_');
+                mybloon = false;
+            }
+            else if (c == '-')
+                mybloon = true;
+            else if (Character.isISOControl(c))
+                mystring.append("CTRL");
+            else if ((c < 0x3B1 || c > 0x3C9) && Character.isLetter(c)) {
+                if (mybloon)
+                    c = Character.toUpperCase(c);
+                mystring.append(c);
+                mybloon = false;
+            }
+        }
+        return mystring.toString();
     }
 }
